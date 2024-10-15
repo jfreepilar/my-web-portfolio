5ac2a38e28baff8ef611d2add3c7147e58d3c220
Joeffrey <tipsjsp@gmail.com>
2024-10-16 00:01:00 +0800

    Deploying to GitHub Pages

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ const buttons = [
 export default function Header() {
   return (
     <div className='text-white flex flex-col items-center mt-20 mb-14'>
-      <img src='/assets/web-portfolio-photo.png' alt='Profile Picture'
+      <img src='assets/web-portfolio-photo.png' alt='Profile Picture'
       className='mb-12 border-[3px] border-white h-[300px] w-[300px] rounded-full'/>
       <h1 className='text-6xl mb-4'>Joeffrey Pilar</h1>
       <h2 className='text-4xl mb-4'>Front End Web Developer</h2>
